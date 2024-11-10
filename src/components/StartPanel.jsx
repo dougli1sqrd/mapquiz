726907708787e89d21cd7b62c3b4438a3c29a149
Eric Douglass <dougli1sqrd@gmail.com>
2024-11-10 10:08:20 -0800

    fixing a bug in the initial quiz parameters

--- src/components/StartPanel.jsx
+++ src/components/StartPanel.jsx
@@ -1,8 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export const StartPanel = ({ startGame, setInitialStudyTime, setStudyTime, setNumberStudies }) => {
 
     const [visible, setVisible] = useState(true);
+
+    const initDropDownRef = useRef(null)
+    const studyTimeDropDownRef = useRef(null);
+    const numberStudiesDropDownRef = useRef(null);
+
+    useEffect(() => {
+        if (initDropDownRef.current) {
+            setInitialStudyTime(initDropDownRef.current.value);
+        }
+        if (studyTimeDropDownRef.current) {
+            setStudyTime(studyTimeDropDownRef.current.value);
+        }
+        if (numberStudiesDropDownRef.current) {
+            setNumberStudies(numberStudiesDropDownRef.current.value);
+        }
+    }, []);
 
 
     return <div>
@@ -10,7 +26,7 @@ export const StartPanel = ({ startGame, setInitialStudyTime, setStudyTime, setNu
 
         <p>
             <label>Initial Study Time
-                <select id="init-time" defaultValue={30000} onChange={(ev) => { setInitialStudyTime(ev.target.value) }}>
+                <select id="init-time" defaultValue={30000} onChange={(ev) => { setInitialStudyTime(ev.target.value) }} ref={initDropDownRef} >
                     <option value={0}>None</option>
                     <option value={15000}>15 seconds</option>
                     <option value={30000}>30 seconds</option>
@@ -21,7 +37,7 @@ export const StartPanel = ({ startGame, setInitialStudyTime, setStudyTime, setNu
 
         <p>
             <label>Number of Studies
-                <select id="studies" defaultValue={3} onChange={(ev) => { setNumberStudies(ev.target.value) }}>
+                <select id="studies" defaultValue={3} onChange={(ev) => { setNumberStudies(ev.target.value) }} ref={numberStudiesDropDownRef} >
                     <option value={0}>None</option>
                     <option value={1}>1</option>
                     <option value={2}>2</option>
@@ -32,7 +48,7 @@ export const StartPanel = ({ startGame, setInitialStudyTime, setStudyTime, setNu
 
         <p>
             <label>Study Time
-                <select id="study-time" defaultValue={15000} onChange={(ev) => { setStudyTime(ev.target.value) }}>
+                <select id="study-time" defaultValue={15000} onChange={(ev) => { setStudyTime(ev.target.value) }} ref={studyTimeDropDownRef}>
                     <option value={5000}>5 seconds</option>
                     <option value={10000}>10 seconds</option>
                     <option value={15000}>15 seconds</option>
